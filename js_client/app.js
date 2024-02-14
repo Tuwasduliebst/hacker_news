86d2a862b35bc7a4b6a276d83303c4f43c15980b
Steven <steven.inho.jung@gmail.com>
2024-02-14 16:43:05 +0900

    function newsFeed
    글목록 화면을 함수로 축약시켜 라우터가 이 함수를 호출하여 제어할 수 있게 함.

--- js_client/app.js
+++ js_client/app.js
@@ -11,12 +11,29 @@ function getData(url) {
   return JSON.parse(ajax.response);
 }
 
-const newsFeed = getData(NEWS_URL);
-const ul = document.createElement("ul");
+function newsFeed() {
+  const newsFeed = getData(NEWS_URL);
+  const newsList = [];
 
-window.addEventListener("hashchange", function () {
-  const id = location.hash.substr(1);
+  newsList.push("<ul>");
 
+  for (let i = 0; i < 10; i++) {
+    newsList.push(`
+      <li>
+        <a href="#${newsFeed[i].id}">
+          ${newsFeed[i].title} (${newsFeed[i].comments_count})
+        </a>
+      </li>
+    `);
+  }
+
+  newsList.push("</ul>");
+
+  container.innerHTML = newsList.join("");
+}
+
+function newsDetail() {
+  const id = location.hash.substring(1);
   const newsContent = getData(CONTENT_URL.replace("@id", id));
 
   container.innerHTML = `
@@ -26,22 +43,12 @@ window.addEventListener("hashchange", function () {
       <a href="#">목록으로</a>
     </div>
   `;
-});
-
-const newsList = [];
-
-newsList.push("<ul>");
-
-for (let i = 0; i < 10; i++) {
-  newsList.push(`
-    <li>
-      <a href="#${newsFeed[i].id}">
-        ${newsFeed[i].title} (${newsFeed[i].comments_count})
-      </a>
-    </li>
-  `);
 }
 
-newsList.push("</ul>");
+function router() {
+  newsFeed();
+}
 
-container.innerHTML = newsList.join("");
+window.addEventListener("hashchange", router);
+
+router();
